8287d4f0d6cc5ff222baf016c1332038e16ded4c
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2024-10-01 14:04:19 -0400

    Initial Commit

---
+++ Interview/InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewApp.swift
+//  Interview
+//
+//  Created by Yi Ling on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
